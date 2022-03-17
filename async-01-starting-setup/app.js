ff2de30e63c469ca75a8bca296a8bde63b7cadd7
Vahram Grigoryan <ramgrigoryan@gmail.com>
2022-03-17 23:04:30 +0400

    promises and promise chaining

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,15 +1,39 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const currentLocation = (optn) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        resolve(success);
+      },
+      optn
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    resolve("Promise is made");
+    setTimeout(() => {
+      console.log("Timeout is set");
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  let position;
+  currentLocation().then((data) => {
+    position = data;
+    return setTimer(2000);
+  }).then(data=>{
+    console.log(data,position);
+  });
   console.log("Clicked!");
 }
 
